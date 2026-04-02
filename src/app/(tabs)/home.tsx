cfd19f90a331d77c8a00e431b0c56f0f10c8ed00
anacmagalhaes <anacarolinasoaresmagalhaes@gmail.com>
2026-04-02 18:07:42 -0300

    feat: create component greeting and add

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,5 +1,6 @@
 import { Card } from "@/components/Card"
-import { ScrollView, StyleSheet, Text, View } from "react-native"
+import { Greeting } from "@/components/Greeting"
+import { ScrollView, StyleSheet, View } from "react-native"
 
 export default function Home() {
     return (
@@ -7,13 +8,9 @@ export default function Home() {
             contentContainerStyle={{ flexGrow: 1 }}
             showsVerticalScrollIndicator={false} >
             <View style={styles.container}>
-                <Text style={styles.greeting}>Welcome, {""}
-                    <Text style={styles.greetingName}>Carol</Text>
-                </Text>
-                <Text style={styles.descriptionTitle}>You've got 7 tasks to do.</Text>
+                <Greeting label="You've got 7 tasks to do."/>
                 <View style={styles.containerCards}>
                     <Card />
-
                 </View>
             </View>
         </ScrollView>
@@ -25,17 +22,6 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#FFFFFF",
         padding: 32,
-        gap: 8
-    },
-    greeting: {
-        fontSize: 26,
-        fontWeight: "bold"
-    },
-    greetingName: {
-        color: "#3f4caf"
-    },
-    descriptionTitle: {
-        fontSize: 16
     },
     containerCards: {
         marginTop: 20,
---
+++ src/components/Greeting.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from "react-native"
+
+type GreetingProps = {
+    label: string,
+    name?: string,
+}
+
+export function Greeting({ label }: GreetingProps) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.greeting}>Welcome, {""}
+                <Text style={styles.greetingName}>Carol</Text>
+            </Text>
+            <Text style={styles.descriptionTitle}>{label}</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        gap: 8
+    },
+    greeting: {
+        fontSize: 26,
+        fontWeight: "bold"
+    },
+    greetingName: {
+        color: "#3f4caf"
+    },
+    descriptionTitle: {
+        fontSize: 16
+    },
+})
